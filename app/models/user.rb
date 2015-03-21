8c78ac03f9569e5a225ef75ca16fb97a1aa61018
Ian <iantskon@gmail.com>
2015-03-21 13:56:40 -0700

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ActiveRecord::Base
 	has_secure_password
 	validates :password, :length => {:minimum => 8 }
-	has_many :courses
-	has_many :attendances, through: :courses
-	validates :username, uniqueness: true, presence: true
-	
-	def self.authenticate username, password
-    User.find_by_username(username).try(:authenticate, password)
+	validates :email, uniqueness: true, presence: true
+	validates :zip, presence: true
+	validates :first, presence: true
+	validates :last, presence: true
+	validates :education, presence: true
+	validates :education_sub, presence: true
+	validates :occupation, presence: true
+	validates :occupation_sub, presence: true
+
+	def self.authenticate email, password
+    User.find_by_email(email).try(:authenticate, password)
   end
 end
